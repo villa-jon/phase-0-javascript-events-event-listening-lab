b0020b4d590f9823ddcd0d2fafd9c9592b11b6a4
Jonathan Villanueva <villanuevajonathan606@gmail.com>
2021-05-07 22:24:37 -0700

    commit tree 6cd889836059dac5d440eb37103b30bf30f0ab75

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function addingEventListener() {
-    const input = document.getElementById('input');
+  const input = document.getElementById('input');
 
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert); 
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+  
+  input.addEventListener('click', clickAlert); 
 }
+
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+
+//     function clickAlert() {
+//       alert('I was clicked!');
+//     }
+    
+//     input.addEventListener('click', clickAlert); 
+// }
+
+// I'm back let's get into this. 
+// So event listeners as the way I see are ways for 
